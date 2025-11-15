eea98761311638d9c3cb5dc6f4399def101c4bcf
Prakash Jha <126317344+23prakashjha@users.noreply.github.com>
2025-11-15 19:57:05 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,7 @@ export default function Login({ onLoginSuccess }) {
     try {
       setLoading(true);
 
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://quiz-project-yx56.onrender.com/api/auth/login", {
         email,
         password,
       });
